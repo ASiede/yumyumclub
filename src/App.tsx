0d03a64540338c56407bb5510022bd99d5fb38ad
Andrea Siede <andrea.siede@gmail.com>
2022-11-23 20:04:48 -0800

    adds yumyum

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://yumyumclub-api.netlify.app");
+      const result = await response.json();
+      setText(result.hello);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
